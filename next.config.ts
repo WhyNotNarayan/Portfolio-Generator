bf215bb335081e8e9b32e008e5eb8ba008d9c2ad
Narayan Gawade <narayangawade684@gmail.com>
2026-04-04 23:49:00 +0530

    feat: implement core portfolio generation wizard with user authentication and template selection system

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // This tells Next.js to treat Prisma as external package (very important)
+  serverExternalPackages: ["@prisma/client", "pg"],
+
+  // Best configuration for Turbopack + Prisma in Next.js 16
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        ".prisma/client/default": "./node_modules/.prisma/client/default.js",
+        "@prisma/client": "./node_modules/@prisma/client",
+      },
+    },
+  },
 };
 
 export default nextConfig;
--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,10 @@
 import "dotenv/config";
-import { defineConfig } from "prisma/config";
+import { defineConfig, env } from "@prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
+
   datasource: {
-    url: process.env["DATABASE_URL"],
-    directUrl: process.env["DIRECT_URL"],
+    url: env("DIRECT_URL") || env("DATABASE_URL"),
   },
 });
