9614f3fef4697dc0d0f0530deffac4fca7bcea59
Narayan Gawade <narayangawade684@gmail.com>
2026-04-13 21:19:53 +0530

    feat: implement Razorpay payment integration and add BasicTemplate component while cleaning up Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // This tells Next.js to treat Prisma as external package (very important)
   serverExternalPackages: ["@prisma/client", "pg"],
-
-  // Best configuration for Turbopack + Prisma in Next.js 16
-  experimental: {
-    turbo: {
-      resolveAlias: {
-        ".prisma/client/default": "./node_modules/.prisma/client/default.js",
-        "@prisma/client": "./node_modules/@prisma/client",
-      },
-    },
-  },
 };
 
 export default nextConfig;
